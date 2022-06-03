8308e3cc24ed34c24a8984d9cb48f9a4201e59f0
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-04 00:25:57 +0300

    add functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Follow along with the examples here
+function shout(string){
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout(string){
+    console.log(string);
+}
+function logwhisper(string){
+    console.log(string);
+}
+function sayHiToHeadphonedRoommate(answer){
+var ansLowerCase = "I can't hear you!";
+var ansUpperCase = "YES INDEED!";
+var funcString = "I would lovr to!";
+if (answer.toLowerCase(answer)===answer){
+    return ansLowerCase;
+}
+else if (answer.toUpperCase(answer) === answer){
+    return ansUpperCase;
+}
+else if("I would love to!" === answer){
+return funcString;
+}
+}
